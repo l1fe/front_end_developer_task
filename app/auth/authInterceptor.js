166a892ba6c1872946e013bdc73604f2c742dd86
art-grig <artem3.1416@gmail.com>
2016-09-27 17:42:24 +0400

    fixed bug with interceptor params setter

--- app/auth/authInterceptor.js
+++ app/auth/authInterceptor.js
@@ -7,8 +7,10 @@ angular.module('psJwtApp').factory('authInterceptor', function(authToken) {
         request: function(config) {
             var token = authToken.getToken();
             config.params = config.params || {};
-            if (token)
+            console.log(config.url.indexOf('localhost'));
+            if (token && config.url.indexOf('localhost') !== -1) {
                 config.params.sessionId = token;
+            }
             return config;
         }
     };
