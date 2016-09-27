71749653a75d3de3f73aefe55ffccc9f460c4f34
l1fe <igor.naumov.17@gmail.com>
2016-09-27 15:46:57 +0300

    Test commit

--- app/app.js
+++ app/app.js
@@ -11,24 +11,3 @@ var app = angular.module("psJwtApp", [
     "com.2fdevs.videogular.plugins.poster"
 ]);
 
-// let's define the scotch controller that we call up in the about state
-app.controller('scotchController', function($scope) {
-
-    $scope.message = 'test';
-
-    $scope.scotches = [
-        {
-            name: 'Macallan 12',
-            price: 50
-        },
-        {
-            name: 'Chivas Regal Royal Salute',
-            price: 10000
-        },
-        {
-            name: 'Glenfiddich 1937',
-            price: 20000
-        }
-    ];
-
-});
